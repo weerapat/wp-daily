05e9b700f2493e327d89c9484c08990e254970f9
Weerapat Supap <weerapatsu@gmail.com>
2016-01-13 11:16:33 +0700

    Add custom js in child theme

--- wp-content/themes/Newspaper-child/functions.php
+++ wp-content/themes/Newspaper-child/functions.php
@@ -11,46 +11,51 @@
 
  */
 
-
-
-
 /*  ----------------------------------------------------------------------------
     add the parent style + style.css from this folder
  */
-add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles', 1000);
-function theme_enqueue_styles() {
-    wp_enqueue_style('td-theme', get_template_directory_uri() . '/style.css', '', '6.1c', 'all' );
-    wp_enqueue_style('td-theme-child', get_stylesheet_directory_uri() . '/style.css', array('td-theme'), '6.1c', 'all' );
-
+add_action('wp_enqueue_scripts', 'theme_enqueue_styles', 1000);
+function theme_enqueue_styles()
+{
+    wp_enqueue_style('td-theme', get_template_directory_uri().'/style.css', '', '6.1c', 'all');
+    wp_enqueue_style('td-theme-child', get_stylesheet_directory_uri().'/style.css', array('td-theme'), '6.1c', 'all');
 }
 
-add_action( 'admin_bar_menu', 'wp_admin_bar_my_custom_account_menu', 11 );
+add_action('admin_bar_menu', 'wp_admin_bar_my_custom_account_menu', 11);
 
-function wp_admin_bar_my_custom_account_menu( $wp_admin_bar ) {
-$user_id = get_current_user_id();
-$current_user = wp_get_current_user();
-$profile_url = get_edit_profile_url( $user_id );
+function wp_admin_bar_my_custom_account_menu($wp_admin_bar)
+{
+    $user_id = get_current_user_id();
+    $current_user = wp_get_current_user();
+    $profile_url = get_edit_profile_url($user_id);
 
-if ( 0 != $user_id ) {
-/* Add the "My Account" menu */
-$avatar = get_avatar( $user_id, 28 );
-$howdy = sprintf( __('สวัสดี, %1$s'), $current_user->display_name );
-$class = empty( $avatar ) ? '' : 'with-avatar';
+    if (0 != $user_id) {
+        /* Add the "My Account" menu */
+        $avatar = get_avatar($user_id, 28);
+        $howdy = sprintf(__('สวัสดี, %1$s'), $current_user->display_name);
+        $class = empty($avatar) ? '' : 'with-avatar';
 
-$wp_admin_bar->add_menu( array(
-'id' => 'my-account',
-'parent' => 'top-secondary',
-'title' => $howdy . $avatar,
-'href' => $profile_url,
-'meta' => array(
-'class' => $class,
-),
-) );
-
+        $wp_admin_bar->add_menu(array(
+            'id' => 'my-account',
+            'parent' => 'top-secondary',
+            'title' => $howdy.$avatar,
+            'href' => $profile_url,
+            'meta' => array(
+            'class' => $class,
+            ),
+        ));
+    }
 }
-}
 
-function custom_login_logo() {
+function custom_scripts()
+{
+    wp_register_script('custom-script', get_stylesheet_directory_uri().'/assets/js/custom.js', [], false, true);
+    wp_enqueue_script('custom-script');
+}
+add_action('wp_enqueue_scripts', 'custom_scripts', 99);
+
+function custom_login_logo()
+{
     echo '<style type="text/css">
         h1 a { background-image:url(/wp-content/uploads/2015/11/225843_462492960440480_8604634_n.jpg) !important; }
     </style>';
@@ -59,4 +64,4 @@ function custom_login_logo() {
 add_action('login_head', 'custom_login_logo');
 
 /* Disable WordPress Admin Bar for all users but admins. */
-  show_admin_bar(false);
+show_admin_bar(false);
